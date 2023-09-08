182c714e8fbc13a2e301134c37fcbaced873dd48
Lovesh1 <lovesh688@gmail.com>
2023-09-08 16:37:04 +0530

    Folder Structure

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import {Link} from 'react-router-dom'
 
 const Login = () => {
+
+
   return (
     <div className="form">
     <form>
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,28 +1,39 @@
 import {Link} from 'react-router-dom'
+import {useState} from 'react'
 
 const Signup = () => {
+
+let [signUp , setSignUp] = useState({
+            fullName :  "" , 
+            email : "" ,
+            password : ""
+})
+
+
+console.log(signUp)
+
   return (
     <div className="form">
 
 <form>
         <div className="container">
             <label>FullName</label>
-            <input type="text" placeholder="e.g. Lovesh120"></input>
+            <input type="text" placeholder="e.g. Lovesh120" onChange={(e)=> setSignUp({ ...signUp,fullName :  e.target.value})}  ></input>
         </div>
         <div className="container">
             <label>Mail</label>
-            <input type="email" placeholder="e.g. Lovesh120"></input>
+            <input type="email" placeholder="e.g. Lovesh120" onChange={(e)=> setSignUp({...signUp , email : e.target.value})}></input>
         </div>
         <div className="container">
             <label>Password</label>
-            <input type="password" placeholder="atleast 6 characters"></input>
+            <input type="password" placeholder="atleast 6 characters" onChange={(e)=> setSignUp({...signUp,password  : e.target.value })}></input>
         </div>
         <div className="container">
             <label>Confirm Password</label>
             <input type="password" placeholder="atleast 6 characters"></input>
         </div>
         <div className="button-container">
-            <button type="submit">Submit</button>     
+            <button type="submit" >Submit</button>     
         </div>
         <li><Link to='/login' >Login Again</Link></li>
     </form>
