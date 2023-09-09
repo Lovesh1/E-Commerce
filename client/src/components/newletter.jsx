8ca3aac3cfa43e84beab3aa50376d78ec122214b
Lovesh1 <lovesh688@gmail.com>
2023-09-09 19:46:19 +0530

    Replace my old code with new one with new structure

--- client/src/components/newletter.jsx
+++ client/src/components/newletter.jsx
@@ -7,7 +7,7 @@ const newletter = () => {
             <h4>Sign Up For Newsletters</h4>
             <p>Get E-mail updates about our latest shop and <span>special offers</span></p>
         </div>
-        <div className="form">
+        <div className="form1">
             <input type="email" placeholder="ENTER EMAIL"></input>
             <button className="normal">Sign in</button>
         </div>
